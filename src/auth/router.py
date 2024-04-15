c621505cc26a05da08ac3ad4ab062f907f133de5
Nicola <nicolaibrahim969@gmail.com>
2024-04-15 18:51:11 +0200

    First commit

---
+++ src/auth/router.py
@@ -0,0 +1,5 @@
+from fastapi. 
+
+@router.get("/posts/{post_id}", response_model=PostResponse)
+async def get_post_by_id(post: Mapping = Depends(valid_post_id)):
+    return post
--- src/main.py
+++ src/main.py
@@ -19,3 +19,48 @@ def get_application():
 
 
 app = get_application()
+from enum import Enum
+from typing import Annotated
+
+from fastapi import FastAPI, Query
+from pydantic import UUID4, BaseModel
+
+# Instantiate FastAPI app
+app = FastAPI()
+
+
+class Item(BaseModel):
+    id: UUID4
+    name: str
+    description: str
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_as_dict = dict(item)
+    print(item.price)
+    if item.price:
+        item_as_dict.update({"price_w_tax": item.price + item.tax})
+
+    return item_as_dict
+
+
+@app.get("/items/")
+    """
+    This Python function defines an endpoint that reads items with an optional query parameter.
+
+    Args:
+      q (Annotated[list[str], Query()]): The parameter `q` in the `read_items` function is a query parameter that expects a
+    list of strings. It has a default value of an empty list `[]`. This parameter is used to filter items based on the
+    provided list of strings.
+
+    Returns:
+      The code defines a FastAPI endpoint at "/items/" that accepts a query parameter "q" as a list of strings. If no value
+    is provided for "q", it defaults to an empty list. When the endpoint is accessed, it returns a dictionary containing
+    the query parameter "q" with its value.
+    """
+async def read_items(q: Annotated[list[str], Query()] = []):
+    query_items = {"q": q}
+    return query_items
