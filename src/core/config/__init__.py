ee1156abfe1c31e964ecfa45ce08a66763578d09
Nicola <nicolaibrahim969@gmail.com>
2024-04-15 18:46:43 +0200

    First commit

---
+++ src/core/config/__init__.py
@@ -0,0 +1,3 @@
+from .local import settings
+
+__all__ = ["settings"]
---
+++ src/core/config/local.py
@@ -0,0 +1,54 @@
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import AnyHttpUrl, BaseModel, PostgresDsn, validator
+
+
+class Settings(BaseModel):
+    PROJECT_NAME: str
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        """
+        Check the databaase uri, if it is string then return it as correct
+        if is not then constructs the database URI with taken values from
+        Setting models
+
+        Args:
+            v (Optional[str]): The current value of the field being validated (DATABASE_URI in this case).
+            values (Dict[str, Any]): A dictionary containing the current values of all fields in the model.
+
+        Returns:
+            Any: _description_
+
+        """
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+
+settings = Settings()
